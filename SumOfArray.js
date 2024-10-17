cf6b96b0c14e305cd03959b1ef59c30196d9e408
MD NUR ALOM <nuralomhasannur17@gmail.com>
2024-10-17 23:14:49 +0600

    mit exam

---
+++ SumOfArray.js
@@ -0,0 +1,11 @@
+function sumArray(numbers) {
+    let sum = 0;
+    for (let i = 0; i < numbers.length; i++) {
+      sum += numbers[i];
+    }
+    return sum;
+  }
+  
+ 
+  const nums = [1, 2, 3, 4, 5];
+  console.log(sumArray(nums));
